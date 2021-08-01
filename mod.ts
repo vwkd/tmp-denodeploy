62bc7eb40c0aa0966e6f7cdfc8a41ea5b1350404
vwkd <33468089+vwkd@users.noreply.github.com>
2021-08-01 19:34:20 +0300

    basic quotes API

--- mod.ts
+++ mod.ts
@@ -1,47 +1,55 @@
+const quotes = [
+  {quote: "A sunny day.", author: "Jay Three"},
+  {quote: "We need! Yes!", author: "Marack Bomana"},
+]
+
 addEventListener("fetch", (event: FetchEvent) => {
   event.respondWith(handleRequest(event.request));
 });
 
+//todo: use pathname
+//todo: error handling
+//todo: react to content-type
 async function handleRequest(request: Request) {
-  const { pathname } = new URL(request.url);
 
-  if (pathname.startsWith("/text")) {
-    return new Response(`A sunny day`,
-      {
-        headers: {
-          "content-type": "text/plain; charset=utf-8",
-        },
-      }
-    );
-  }
+  if (request.method == "GET") {
 
-  if (pathname.startsWith("/json")) {
-    return new Response(
-      JSON.stringify({
-        message: `A sunny day`,
-      }),
-      {
-        headers: {
-          "content-type": "application/json; charset=utf-8",
-        },
-      }
-    );
-  }
-
-  return new Response(
-    `<html>
-      <head>
-        <link rel="stylesheet" href="style.css" />
-      </head>
-      <body>
-        <h1>Example</h1>
-        <p>Hello World</p>
-      </body>
-    </html>`,
-    {
+    const quote = getQuote();
+    return new Response(JSON.stringify(quote),{
       headers: {
-        "content-type": "text/html; charset=utf-8",
-      },
-    },
-  );
+        "content-type": "application/json; charset=utf-8"
+      }
+    })
+
+  } else if (request.method == "POST") {
+
+    const json = await request.json();
+    const quote = setQuote(json);
+    return new Response(JSON.stringify(quote),{
+      headers: {
+        "content-type": "application/json; charset=utf-8"
+      }
+    })
+   
+  } else {
+
+    return new Response(null, {
+      status: 405,
+      statusText: "Method Not Allowed",
+    });
+    
+  }
+}
+
+// todo: error handling
+function getQuote() {
+  const index = Math.floor(Math.random() * quotes.length);
+  return quotes[index];
+}
+
+// todo: error handling
+// todo: validate input
+function setQuote(json) {
+  quotes.push(json);
+  return json;
 }
